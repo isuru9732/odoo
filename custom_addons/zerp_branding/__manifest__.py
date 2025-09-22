2233fafcfbab627bbf6c92bf489221304df2feb9
Isuru Ruwantha <isr.ruwantha@gmail.com>
2025-09-22 13:23:17 +0530

    ODOO-BRANDING

---
+++ custom_addons/zerp_branding/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "ZERP Branding",
+    "version": "1.0",
+    "summary": "Custom branding for Odoo",
+    "author": "Your Company",
+    "category": "Branding",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "zerp_branding/static/src/img/zerp-logo.png",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
